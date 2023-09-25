b91ead601b4d27151fb990d1b18c7b76b3bbe5f5
Dmitry Liapin <ldemetrios@yandex.ru>
2023-09-25 13:16:07 +0300

    Add IntList samples

---
+++ src/main/kotlin/ArrayIntList.kt
@@ -0,0 +1,69 @@
+import java.util.*
+
+class ArrayIntList {
+    private var arr = IntArray(16)
+    private var capacity = 16
+    var size: Int = 0
+        private set
+
+    operator fun contains(element: Int): Boolean {
+        for (i in 0 until size) if (arr[i] == element) return true
+        return false
+    }
+
+    fun containsAll(elements: Collection<Int>): Boolean {
+        for (el in elements) {
+            if (!contains(el)) return false
+        }
+        return true
+    }
+
+    operator fun get(index: Int): Int {
+        if (index < 0) throw ArrayIndexOutOfBoundsException("$index")
+        if (index >= size) return 0
+        return arr[index]
+    }
+
+    fun indexOf(element: Int): Int {
+        for (i in 0 until size) if (arr[i] == element) return i
+        return -1
+    }
+
+    fun isEmpty(): Boolean = size == 0
+
+    fun lastIndexOf(element: Int): Int {
+        for (i in (size - 1) downTo 0) if (arr[i] == element) return i
+        return -1
+    }
+
+    fun add(element: Int) {
+        if (size == capacity) {
+            arr = Arrays.copyOf(arr, capacity + capacity / 2 + 1)
+            capacity = arr.size
+        }
+        arr[size] = element
+        size++
+    }
+
+    fun addAll(elements: Collection<Int>) {
+        if (capacity < size + elements.size) {
+            arr = Arrays.copyOf(arr, maxOf(capacity + capacity / 2 + 1, size + elements.size))
+            capacity = arr.size
+        }
+        var i = size
+        for (el in elements) set(i++, el)
+    }
+
+    fun clear() {
+        size = 0
+    }
+
+    operator fun set(index: Int, element: Int) {
+        if (capacity <= index) {
+            arr = Arrays.copyOf(arr, maxOf(capacity + capacity / 2 + 1, index))
+            capacity = arr.size
+        }
+        arr[index] = element
+        if (index <= size) size = index + 1
+    }
+}
---
+++ src/main/kotlin/ChunkedIntList.kt
@@ -0,0 +1,107 @@
+
+class ChunkedIntList {
+    private var arr: Array<Array<Array<IntArray?>?>?>? = null
+    var size: Int = 0
+        private set
+
+    operator fun contains(element: Int): Boolean {
+        if (arr == null) return false
+        for (i in 0 until 256) {
+            if (arr!![i] == null) continue
+            for (j in 0 until 256) {
+                if (arr!![i]!![j] == null) continue
+                for (k in 0 until 256) {
+                    if (arr!![i]!![j]!![k] == null) continue
+                    for (l in 0 until 256) {
+                        if (arr!![i]!![j]!![k]!![l] == element) return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+
+    fun containsAll(elements: Collection<Int>): Boolean {
+        for (el in elements) {
+            if (!contains(el)) return false
+        }
+        return true
+    }
+
+    operator fun get(index: Int): Int {
+        if (index < 0) throw ArrayIndexOutOfBoundsException("$index")
+        if (index >= size) return 0
+        return arr?.get(index shr 24)?.get(index shr 16 and 255)?.get(index shr 8 and 255)?.get(index and 255) ?: 0
+    }
+
+    fun indexOf(element: Int): Int {
+        if (arr == null) return -1
+        for (i in 0 until 256) {
+            if (arr!![i] == null) continue
+            for (j in 0 until 256) {
+                if (arr!![i]!![j] == null) continue
+                for (k in 0 until 256) {
+                    if (arr!![i]!![j]!![k] == null) continue
+                    for (l in 0 until 256) {
+                        if (arr!![i]!![j]!![k]!![l] == element) return (i shl 24) + (j shl 16) + (k shl 8) + l
+                    }
+                }
+            }
+        }
+        return -1
+    }
+
+    fun isEmpty(): Boolean = size == 0
+
+    fun lastIndexOf(element: Int): Int {
+        if (arr == null) return -1
+        for (i in 255 downTo 0) {
+            if (arr!![i] == null) continue
+            for (j in 255 downTo 0) {
+                if (arr!![i]!![j] == null) continue
+                for (k in 255 downTo 0) {
+                    if (arr!![i]!![j]!![k] == null) continue
+                    for (l in 255 downTo 0) {
+                        if (arr!![i]!![j]!![k]!![l] == element) return (i shl 24) + (j shl 16) + (k shl 8) + l
+                    }
+                }
+            }
+        }
+        return -1
+    }
+
+    fun add(element: Int) {
+        val i = size shr 24
+        val j = size shr 16 and 255
+        val k = size shr 8 and 255
+        val l = size and 255
+        if (arr == null) arr = Array(256) { null }
+        if (arr!![i] == null) arr!![i] = Array(256) { null }
+        if (arr!![i]!![j] == null) arr!![i]!![j] = Array(256) { null }
+        if (arr!![i]!![j]!![k] == null) arr!![i]!![j]!![k] = IntArray(256)
+        arr!![i]!![j]!![k]!![l] = element
+        size++
+    }
+
+    fun addAll(elements: Collection<Int>) {
+        for (el in elements) add(el)
+    }
+
+    fun clear() {
+        size = 0
+        arr = null
+    }
+
+    operator fun set(index: Int, element: Int) {
+        val i = index shr 24
+        val j = index shr 16 and 255
+        val k = index shr 8 and 255
+        val l = index and 255
+        if (arr == null) arr = Array(256) { null }
+        if (arr!![i] == null) arr!![i] = Array(256) { null }
+        if (arr!![i]!![j] == null) arr!![i]!![j] = Array(256) { null }
+        if (arr!![i]!![j]!![k] == null) arr!![i]!![j]!![k] = IntArray(256)
+        arr!![i]!![j]!![k]!![l] = element
+        if (index >= size) size = index + 1
+    }
+}
